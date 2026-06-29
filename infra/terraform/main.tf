06ae45bb8d0a9fcc8e8cb989ba33ee2dfd2dfcf4
allabaksh279@gmail.com <allabakshu279@gmail.com>
2026-06-29 19:59:31 +0530

    Changes for Deployment fixed

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -4,24 +4,24 @@ resource "azurerm_resource_group" "main" {
   
 }
 
-# module "aks" {
-#   source  = "Azure/aks/azurerm"
-#   version = "11.0.0"
-#   # insert the 2 required variables here
-#   resource_group_name   = var.resource_group_info.name
-#   cluster_name          = var.aks_cluster_info.name
-#   location              = var.resource_group_info.location
+ module "aks" {
+   source  = "Azure/aks/azurerm"
+   version = "11.0.0"
+   # insert the 2 required variables here
+   resource_group_name   = var.resource_group_info.name
+   cluster_name          = var.aks_cluster_info.name
+   location              = var.resource_group_info.location
 
-#   agents_count          = var.aks_cluster_info.count
-#   agents_size           = var.aks_cluster_info.size
-#   kubernetes_version    = var.aks_cluster_info.version
-#   network_plugin        = "azure"
-#   os_sku                = "Ubuntu"
-#   sku_tier              = "Standard"
+   agents_count          = var.aks_cluster_info.count
+   agents_size           = var.aks_cluster_info.size
+   kubernetes_version    = var.aks_cluster_info.version
+   network_plugin        = "azure"
+   os_sku                = "Ubuntu"
+   sku_tier              = "Standard"
 
-#   depends_on = [ azurerm_resource_group.main ]
+   depends_on = [ azurerm_resource_group.main ]
 
-# }
+ }
 
 
 resource "azurerm_kubernetes_cluster" "aks" {
